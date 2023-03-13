81c05d8990caadf24cda1f8c17e62b4b79ea4833
NguyenDucAnh <ducanhkhtn@gmail.com>
2023-03-13 15:25:48 +0700

    update font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,22 @@
 module.exports = {
-  content: [
-    './src/**/*.html',
-    './src/**/*.js',
-  ],
+  content: ["./src/**/*.html", "./src/**/*.js"],
   theme: {
     extend: {
       zIndex: {
-        '-1': '-1',
+        "-1": "-1",
       },
       flexGrow: {
-        '5' : '5'
-      }
+        5: "5",
+      },
+      fontFamily: {
+        avantGardeRegular: ["'VIEAvantGardeRegular'", "sans-serif"],
+        avantGardeBold: ["'VIEAvantGardeBold'", "sans-serif"],
+        avantGardeSemibold: ["'VIEAvantGardeDemibold'", "sans-serif"],
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
